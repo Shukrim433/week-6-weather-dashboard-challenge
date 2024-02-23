b1238dfafcb38eb0b0778ea4020792ce5aa3167e
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-02-23 16:42:14 +0000

    added the javascript starter code file to my assets folder (starter.js)

---
+++ assets/starter.js
@@ -0,0 +1,36 @@
+const WEATHER_API_BASE_URL = 'https://api.openweathermap.org';
+const WEATHER_API_KEY = 'f23ee9deb4e1a7450f3157c44ed020e1';
+const MAX_DAILY_FORECAST = 5;
+
+// Lookup the location to get the Lat/Lon
+function doSomething() {
+
+    var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+
+            // Pick the First location from the results
+            //const location = data[0];
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+
+            // Get the Weather for the cached location
+            var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
+            
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+
+                    console.log(data);
+
+                    // Show the Current Weather Forecast
+                    
+
+                    // Show the 5 Day Weather Forecast
+                    
+                })
+            })
+}
